9839fc15461089f92337d2697b287ca871914faa
Saikumar2323235 <mvvsaikumar23@gmail.com>
2026-03-24 02:18:06 +0530

    Added python file

---
+++ varaiable.py
@@ -0,0 +1,57 @@
+''''a=10
+print(a)
+
+a,b,c=1
+print(a)
+
+a,b,c=1,2,3
+print(a,b,c)
+
+a=b=c=1
+print(b)
+print(a)
+print(c)
+
+a=1,2,3
+print(a)
+
+abc=1
+print(a)
+
+case sensitive= we have to start with alphabets avoiding digit, after writing alphabets we can write with digits but we dont use digits from starting
+
+
+we have to use id for finding memory location
+abc1234=1
+print(id(abc1234))
+
+codegnan=1
+CODEGNAN=1
+print(codegnan)
+print (CODEGNAN)
+
+data types
+int= 1,-1
+float=1.22 decimal point
+boolean= false=0 true =1
+string= 'saikumar' single codes double codes
+complex =2+1i real and imaginary
+
+a=1
+b=-2
+print(type(a),type(b))
+
+a=True
+b=False
+print(type(a),type(b))
+
+print (True==1)
+
+c=1.2
+print(type(c))
+
+a=1
+print(int(a))
+'''
+data lose = explicity type conversion
+data no loss = implicity type conversion
